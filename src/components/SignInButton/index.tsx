04fadb22496b61aa6c16b37387a6164127cd9868
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2021-04-07 16:12:23 -0300

    Backend ignews em desenvolvimento

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,29 @@
 import {FaGithub} from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import {signIn, signOut, useSession} from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-  const isUserLoggedIn = false;
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
-
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
 
       <FaGithub color="#04d361" />
 
-      Luiz Carlos Ribeiro
+      {session.user.name}
 
-      <FiX color="#737380" className={styles.closeIcon} />
+      <FiX color="#738075" className={styles.closeIcon} />
 
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
 
+    <button type="button" 
+      className={styles.signInButton} 
+      onClick={() => signIn('github')}
+    >
 
       <FaGithub color="#eba417" />
 
